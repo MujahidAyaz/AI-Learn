7d15c9de56eef56bc47f18a3a47dfea8d7eb7fc6
Mujahid Ayaz <mujahid.ayaz1@gmail.com>
2026-07-17 03:02:03 -0700

    Add checkpoint module for saving trained model weights

---
+++ Week 5/Deep-Learning/PyTorch/Fashion-MNIST Image Classification using PyTorch/src/checkpoint.py
@@ -0,0 +1,46 @@
+"""
+============================================================
+Checkpoint Module
+
+This module contains helper functions for saving and loading
+PyTorch model checkpoints.
+============================================================
+"""
+
+# ==========================================================
+# Import Libraries
+# ==========================================================
+
+import torch
+from pathlib import Path
+
+from configs.config import MODEL_DIR
+
+
+# ==========================================================
+# Save Checkpoint
+# ==========================================================
+
+def save_checkpoint(model, filename="best_model.pth"):
+    """
+    Saves only the model weights.
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        Trained model.
+
+    filename : str
+        Name of checkpoint file.
+    """
+
+    checkpoint_path = MODEL_DIR / filename
+
+    torch.save(
+        model.state_dict(),
+        checkpoint_path
+    )
+
+    print(f"\nModel saved successfully!")
+
+    print(checkpoint_path)
